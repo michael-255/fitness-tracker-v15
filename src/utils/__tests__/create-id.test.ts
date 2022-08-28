bd7fe198fc4ba84483fa84baa72fad8c7557950e
michaeljoy255 <kodiakchaos@gmail.com>
2022-08-28 18:45:34 -0400

    removing old build-id util

--- src/utils/__tests__/create-id.test.ts
+++ src/utils/__tests__/create-id.test.ts
@@ -1,51 +1,45 @@
 import { describe, test, expect } from 'vitest'
-import { buildIdFunction } from '@/utils/build-id'
+import { uuid, customId } from '@/utils/create-id'
 
-describe('buildIdFunction', () => {
+describe('customId', () => {
   test('should throw error if segmentLengths param has less then 1 element', () => {
     expect(() => {
-      buildIdFunction({ segmentLengths: [] })
+      customId({ segmentLengths: [] })
     }).toThrow()
   })
 
   test('should throw error if segmentLengths param has any non-positive numbers', () => {
     expect(() => {
-      buildIdFunction({ segmentLengths: [-9, 8] })
+      customId({ segmentLengths: [-9, 8] })
     }).toThrow()
     expect(() => {
-      buildIdFunction({ segmentLengths: [8, 0] })
+      customId({ segmentLengths: [8, 0] })
     }).toThrow()
     expect(() => {
-      buildIdFunction({ segmentLengths: [8, 0.999, 8] })
+      customId({ segmentLengths: [8, 0.999, 8] })
     }).toThrow()
   })
 
   test('should throw error if delimiter param is not a string of length 1 or empty', () => {
     expect(() => {
-      buildIdFunction({ delimiter: 'invalid-length' })
+      customId({ delimiter: 'invalid-delimiet' })
     }).toThrow()
   })
 
-  test('should return a function when called with valid parameters', () => {
-    const idFunction = buildIdFunction()
-    expect(typeof idFunction).toBe('function')
-    expect(idFunction).toEqual(expect.any(Function))
-  })
-
-  test('should create an id function that creates ids based on build function params', () => {
-    const defaultId = buildIdFunction()()
-    const bigId = buildIdFunction({
+  test('should create a custom id based on the params ', () => {
+    const defaultId = customId()
+    const bigId = customId({
       segmentLengths: [2, 2, 2, 2, 2, 2],
       delimiter: ':',
-    })()
-    const packedId = buildIdFunction({
+    })
+    const packedId = customId({
       segmentLengths: [8, 8, 8],
       delimiter: '',
-    })()
-    const upcId = buildIdFunction({
+    })
+    const upcId = customId({
       segmentLengths: [2, 8, 8, 2],
       delimiter: '_',
-    })()
+    })
 
     expect(defaultId).toMatch(/[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}/)
     expect(bigId).toMatch(/[A-Z0-9]{2}:[A-Z0-9]{2}:[A-Z0-9]{2}:[A-Z0-9]{2}:[A-Z0-9]{2}:[A-Z0-9]{2}/)
